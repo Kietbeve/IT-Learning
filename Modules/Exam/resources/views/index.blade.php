6cea9605ca3db1b6bcb3b9e4b153ad7c8017035a
Kietbeve <nguyentuankiet0618@gmail.com>
2026-06-09 16:12:47 +0700

    refactor

--- Modules/Exam/resources/views/index.blade.php
+++ Modules/Exam/resources/views/index.blade.php
@@ -5,12 +5,12 @@
         <h1 class="text-3xl font-bold text-gray-900 mb-6">Danh sách bài thi</h1>
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            @include('exam::layouts.patials.exam_card')
-            @include('exam::layouts.patials.exam_card')
-            @include('exam::layouts.patials.exam_card')
-            @include('exam::layouts.patials.exam_card')
-            @include('exam::layouts.patials.exam_card')
-            @include('exam::layouts.patials.exam_card')
+            @include('exam::patials.exam_card')
+            @include('exam::patials.exam_card')
+            @include('exam::patials.exam_card')
+            @include('exam::patials.exam_card')
+            @include('exam::patials.exam_card')
+            @include('exam::patials.exam_card')
         </div>
     </div>
 @endsection
