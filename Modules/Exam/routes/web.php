5e5985ddc14089d0cd6e36f4a49be57bbc6dad55
Kietbeve <nguyentuankiet0618@gmail.com>
2026-06-12 18:22:45 +0700

    setup: thêm Middleware role

--- Modules/Exam/routes/web.php
+++ Modules/Exam/routes/web.php
@@ -36,6 +36,7 @@ Route::group(["prefix"=> "exam","middleware"=> "auth"], function () {
 
 //route auth + contributor
 Route::group(["prefix"=> "contributor","middleware"=> ["auth",]], function () {
+//Route::group(["prefix"=> "contributor","middleware"=> ["auth","role:admin|contributor"] ], function () {//test role
     Route::get('questions',[ExamController::class,"questionManager"])->name('contributor.questions');
     Route::get('exams',[ExamController::class,"examManager"])->name('contributor.exams');
     // Route::get('exams/{examId}/detail', [ExamController::class, 'examDetail'])->name('contributor.exams.detail');
