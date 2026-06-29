b6a4413568ccb1ded11a357bdb4d79d52ccf509c
Kietbeve <nguyentuankiet0618@gmail.com>
2026-06-29 21:51:26 +0700

    Revert "update: cài đặt alprineJS"
    This reverts commit f2402b6c466a889ded867f43a90ce24a36f0e2c9.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,2 @@
 import './bootstrap';
-// import '../../vendor/power-components/livewire-powergrid/resources/js/powergrid';
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+import '../../vendor/power-components/livewire-powergrid/resources/js/powergrid';
