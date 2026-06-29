6896ac3a13f88cb6d6250b2265978283d6789ec0
GiaKiet <giakietlehoang@gmail.com>
2026-06-29 22:27:18 +0700

    UI: nâng cấp trang exam_detail

--- Modules/Exam/resources/views/partials/exam_card.blade.php
+++ Modules/Exam/resources/views/partials/exam_card.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white rounded-2xl border border-gray-200 shadow-sm hover:shadow-lg hover:border-blue-900 hover:-translate-y-1 transition-all duration-300 overflow-hidden h-full flex flex-col">
     {{-- Header --}}
-    <div class="bg-linear-to-r from-indigo-500 to-indigo-900 p-5">
+    <div class="bg-linear-to-r from-blue-700 via-blue-600 to-cyan-500 p-5">
 
         <div class="flex items-center justify-between">
 
