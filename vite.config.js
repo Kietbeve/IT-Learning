11eb32e249e847012531e37b16377b0426b26067
Kietbeve <nguyentuankiet0618@gmail.com>
2026-07-18 23:20:10 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'Modules/Exam/resources/assets/js/exam-security.js', ],
             refresh: true,
         }),
         tailwindcss(),
